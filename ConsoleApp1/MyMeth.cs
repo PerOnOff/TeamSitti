c14c30896ae6e1df7de2b252eaffcdb4aa1886b9
the.pero.forever@gmail.com <the.pero.forever@gmail.com>
2020-11-02 18:35:55 +0100

    Add project files.

---
+++ ConsoleApp1/MyMeth.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Pood.TeamCitiTest
+{
+    public static class MyMeth
+    {
+        public static double Suqared(double x) {
+            return x * x;
+        }
+    }
+}
---
+++ UnitTestProject1/TestSuqared.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vsite.Pood.TeamCitiTest
+{
+    [TestClass]
+    public class TestSuqared
+    {
+        [TestMethod]
+        public void SuquaredReturns4forArgument2()
+        {
+            Assert.AreEqual(4, MyMeth.Suqared(2));
+        }
+
+        [TestMethod]
+        public void SuquaredReturns0forArgument0()
+        {
+            Assert.AreEqual(0, MyMeth.Suqared(0));
+        }
+
+        [TestMethod]
+        public void SuquaredReturns4forArgumentMinus2()
+        {
+            Assert.AreEqual(4, MyMeth.Suqared(-2));
+        }
+    }
+}
